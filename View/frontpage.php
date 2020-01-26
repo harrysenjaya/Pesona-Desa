763b6651fdfb8d1bb4b3e1095c4721fb91ed1e4e
Harry Senjaya <harxsenjaya56@gmail.com>
2020-01-26 17:07:46 +0700

    Update frontpage.php

--- View/frontpage.php
+++ View/frontpage.php
@@ -24,7 +24,7 @@
     <div class="font" style="float: right;font-size: 30px; margin-top: 20px; margin-right: 50px;">
         <i class="fa fa-user w3-text-white"></i>
         <a id="loginText" style="text-decoration: none;">
-            <p class="mainLogin" style="display: inline; color: white; margin-right: 10px; font-size: 25px; cursor:pointer;">Sign in</p>
+            <p class="mainLogin" style="display: inline; color: white; margin-right: 10px; font-size: 25px; cursor:  pointer;">Sign in</p>
         </a>
     </div>
     <div class="clearFloat"></div>
