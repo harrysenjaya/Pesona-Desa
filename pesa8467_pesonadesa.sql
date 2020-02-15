a66bcdd2f035e1caf4b0b1ad81f82e5af55f0c24
Harry Senjaya <harxsenjaya56@gmail.com>
2020-02-15 23:31:07 +0700

    database

--- pesa8467_pesonadesa.sql
+++ pesa8467_pesonadesa.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Waktu pembuatan: 28 Jan 2020 pada 15.44
+-- Waktu pembuatan: 15 Feb 2020 pada 17.30
 -- Versi server: 10.1.38-MariaDB
 -- Versi PHP: 7.3.3
 
@@ -25,12 +25,32 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
+-- Struktur dari tabel `stay`
+--
+
+CREATE TABLE `stay` (
+  `Id` int(10) NOT NULL,
+  `Nama Desa` varchar(100) NOT NULL,
+  `Foto` varchar(100) NOT NULL,
+  `Deskripsi` varchar(1000) NOT NULL,
+  `Tempat Tidur` varchar(100) NOT NULL,
+  `Fasilitas` varchar(1000) NOT NULL,
+  `Availability` date NOT NULL,
+  `Review` varchar(1000) NOT NULL,
+  `Aturan Tempat` varchar(1000) NOT NULL,
+  `Objek Wisata Sekitar` varchar(1000) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
 -- Struktur dari tabel `user`
 --
 
 CREATE TABLE `user` (
   `FirstName` varchar(50) NOT NULL,
   `LastName` varchar(50) NOT NULL,
+  `password` varchar(50) NOT NULL,
   `JenisKelamin` char(1) NOT NULL,
   `TanggalLahir` date NOT NULL,
   `Email` varchar(100) NOT NULL,
@@ -39,14 +59,39 @@ CREATE TABLE `user` (
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
+-- Dumping data untuk tabel `user`
+--
+
+INSERT INTO `user` (`FirstName`, `LastName`, `password`, `JenisKelamin`, `TanggalLahir`, `Email`, `Telepon`, `Alamat`) VALUES
+('Harry', '', '', '', '0000-00-00', '', 0, ''),
+('Tes', 'Tes', 'e10adc3949ba59abbe56e057f20f883e', 'L', '2019-12-04', 'tes1@yahoo.com', 123456, 'tes'),
+('Tes', 'TES', '123456', 'L', '2019-12-04', 'tes@yahoo.com', 123456, 'tes');
+
+--
 -- Indexes for dumped tables
 --
+
+--
+-- Indeks untuk tabel `stay`
+--
+ALTER TABLE `stay`
+  ADD PRIMARY KEY (`Id`);
 
 --
 -- Indeks untuk tabel `user`
 --
 ALTER TABLE `user`
   ADD PRIMARY KEY (`Email`);
+
+--
+-- AUTO_INCREMENT untuk tabel yang dibuang
+--
+
+--
+-- AUTO_INCREMENT untuk tabel `stay`
+--
+ALTER TABLE `stay`
+  MODIFY `Id` int(10) NOT NULL AUTO_INCREMENT;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
