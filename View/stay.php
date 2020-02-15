3fd6b7b6af7cda4368fea3822fde46462c4f4c31
Harry Senjaya <harxsenjaya56@gmail.com>
2020-02-16 00:27:11 +0700

    Update stay.php

--- View/stay.php
+++ View/stay.php
@@ -1,11 +1,11 @@
 <?php
 echo "<div>";
 for($i=0; $i<sizeof($data); $i++){
-    echo $data[0]['Id'];
+    $foto = $data[0]['Foto'];
+    echo "<img src=Image/$foto.jpg>";
     echo "<br>";
     echo $data[0]['NamaDesa'];
     echo "<br>";
-    echo $data[0]['Foto'];
 }
 echo "</div>";
 ?>
