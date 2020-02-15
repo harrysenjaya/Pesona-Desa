d45e1b24ba4f5b626fe11e325747629f2d93fa98
Harry Senjaya <harxsenjaya56@gmail.com>
2020-02-16 00:18:33 +0700

    stay uda bisa

--- Controller/view.php
+++ Controller/view.php
@@ -20,5 +20,21 @@
             ob_end_clean();
             return $content;
         }
+
+        public static function createView2($view,$param){
+            foreach ($param as $key => $value) {
+                $$key=$value;
+            }
+            ob_start();
+            include 'View/'.$view;
+            $content = ob_get_contents();
+            ob_end_clean();
+            ob_start();
+            include 'View/layout/header.php';
+            $head = ob_get_contents();
+            ob_end_clean();
+            echo $head;
+            echo $content;
+        }
     }
 ?>
--- View/stay.php
+++ View/stay.php
@@ -1,1 +1,11 @@
-<h1> TES</h1>
+<?php
+echo "<div>";
+for($i=0; $i<sizeof($data); $i++){
+    echo $data[0]['Id'];
+    echo "<br>";
+    echo $data[0]['NamaDesa'];
+    echo "<br>";
+    echo $data[0]['Foto'];
+}
+echo "</div>";
+?>
