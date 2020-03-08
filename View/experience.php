c4e2d6151fbcd83d02316f979cd23ab2d0c23f49
Harry Senjaya <harxsenjaya56@gmail.com>
2020-03-09 00:12:08 +0700

    Experience dikit lagi

--- View/experience.php
+++ View/experience.php
@@ -1,1 +1,17 @@
-<h1>INI EXP</h1>
+<?php
+console.log($data);
+echo "<div>";
+for($i=0; $i<sizeof($data); $i++){
+    $foto = $data[0]['Foto'];
+    echo "<img src=Image/$foto.jpg>";
+    echo "<br>";
+    echo $data[0]['Brand'];
+    echo "<br>";
+    echo "Nama Desa : ";
+    echo $data[0]['NamaDesa'];
+    echo "<br>";
+    echo "Harga  : ";
+    echo $data[0]['Harga'];
+}
+echo "</div>";
+?>
